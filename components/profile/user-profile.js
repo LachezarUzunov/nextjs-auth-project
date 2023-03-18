571236d2f44126c2088ad7cc5056b6be9c1df151
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-18 15:43:44 +0200

    change pass functionality added

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,13 +1,25 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
   // Redirect away if NOT auth
 
+  const changePasswordHandler = async (passwordData) => {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
